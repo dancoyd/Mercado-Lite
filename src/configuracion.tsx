866cebad516ca294c996a6d43362681128d53eab
dancoyd <dangertrudis@outlook.com>
2025-08-05 15:11:47 -0300

    con mas iconos

--- src/configuracion.tsx
+++ src/configuracion.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiArrowLeft, FiLock, FiBell, FiSun } from 'react-icons/fi';
 import './configuracion.css';
 
 const Configuracion = () => {
   return (
     <div className="configuracion-container">
       <div className="volver-wrapper">
-        <Link to="/opciones" className="volver-flecha">←</Link>
+        <Link to="/opciones" className="volver-flecha">
+          <FiArrowLeft className="icono-volver" />
+        </Link>
       </div>
 
       <h2 className="configuracion-titulo">Configuración</h2>
 
       <ul className="configuracion-lista">
-        <Link to="/contrasena" className="config-btn">Contrasena</Link>
-        <Link to="/notificaciones" className="config-btn">Notificaciones</Link>
-        <li><button className="config-btn">Tema</button></li>
+        <li>
+          <Link to="/contrasena" className="config-btn">
+            <FiLock className="config-icon" />
+            Contraseña
+          </Link>
+        </li>
+        <li>
+          <Link to="/notificaciones" className="config-btn">
+            <FiBell className="config-icon" />
+            Notificaciones
+          </Link>
+        </li>
+        <li>
+          <button className="config-btn">
+            <FiSun className="config-icon" />
+            Tema
+          </button>
+        </li>
       </ul>
     </div>
   );
