1f32fb3dc31837a9d1b612d3260d192dd47a43a5
Valentin-Alfie <48178972@est.ort.edu.ar>
2025-08-10 22:49:34 -0300

    Backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
 
-app.use(express.json());
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error en la conexión");
+  }
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
